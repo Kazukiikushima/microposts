b40da9878b08f58c73961ce617673141f86df004
KazukiIkushima <rupt30061opt5fx@yahoo.co.jp>
2016-10-13 04:41:06 +0000

    user-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update]
+  
   def show # 追加
    @user = User.find(params[:id])
   end
@@ -16,11 +19,32 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    unless session[:user_id] == @user.id
+      flash[:danger] = "you did not login this user"
+      redirect_to @user
+    end
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = "プロフィールを編集しました"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :email, :place, :birthday, :comment,:password,
                                  :password_confirmation)
   end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
 end
